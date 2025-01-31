c9be2c4899dd8fb6b0c7155f4d0eb09f7cfad8e8
openSongce <rkddkwl059@naver.com>
2025-01-31 16:43:54 +0900

    [Bronze V] Title: N 찍기, Time: 1148 ms, Memory: 36572 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            System.out.println(i);
+        }
+    }
+}
