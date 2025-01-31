ad80c2f4136594d369a77a600741f787eb9d19fe
openSongce <rkddkwl059@naver.com>
2025-01-31 16:54:47 +0900

    [Bronze IV] Title: 기찍 N, Time: 1040 ms, Memory: 34444 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = n; i > 0; i--){
+            System.out.println(i);
+        }
+    }
+}
