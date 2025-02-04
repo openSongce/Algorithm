33d73ff15068310065f107ab2ae8470bdf81cd02
openSongce <rkddkwl059@naver.com>
2025-02-04 09:07:10 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 64 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String temp = br.readLine();
+        System.out.print(temp.length());
+    }
+}
